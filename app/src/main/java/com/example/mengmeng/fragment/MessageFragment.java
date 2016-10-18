3f4f3f5a1d11448d551af0cd05bfdb01d2701199
luxiansenv5 <1216189265@qq.com>
2016-10-18 16:27:05 +0800

    author:wmf desc:change

--- app/src/main/java/com/example/mengmeng/fragment/MessageFragment.java
+++ app/src/main/java/com/example/mengmeng/fragment/MessageFragment.java
@@ -1,10 +1,25 @@
 package com.example.mengmeng.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mengmeng.activity.R;
+
 /**
  * Created by admin on 2016/10/17.
  */
 public class MessageFragment extends BaseFragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_layout_message,null);
+
+        return v;
+    }
 
     @Override
     public void initView() {
