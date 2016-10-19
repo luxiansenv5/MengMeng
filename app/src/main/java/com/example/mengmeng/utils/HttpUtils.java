d2a5fabcc17e195edab21279774ca77b479fe485
陆猛 <2606520633@qq.com>
2016-10-19 14:32:03 +0800

    author:陆猛

--- app/src/main/java/com/example/mengmeng/utils/HttpUtils.java
+++ app/src/main/java/com/example/mengmeng/utils/HttpUtils.java
@@ -5,7 +5,7 @@ package com.example.mengmeng.utils;
  */
 public class HttpUtils {
 
-    public static final String HOST="http://10.40.5.12:8080/web/";
+    public static final String HOST="http://10.40.5.12:8080/mmweb/";
 
     public static final String  HOST_COMMUNICATIE = "http://10.0.2.2:8080/MMAPP/";
 }
