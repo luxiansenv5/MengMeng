e49ea12d80489251acd4b0b8c64ddbae320ac115
luxiansenv5 <1216189265@qq.com>
2016-10-21 11:25:41 +0800

    author:武梦凡

--- app/src/main/java/application/MyApplication.java
+++ app/src/main/java/application/MyApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
+import io.rong.imkit.RongIM;
+
 
 /**
  * Created by 程和 on 2016/10/14.
@@ -15,6 +17,7 @@ public class MyApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        RongIM.init(this);
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
     }
