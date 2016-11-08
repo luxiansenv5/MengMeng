b42f69740839d389c1880360bb26b4e659d9de3a
luxiansenv5 <1216189265@qq.com>
2016-11-08 19:52:07 +0800

    Merge branch 'master' of https://github.com/luxiansenv5/MengMeng
    Conflicts:
    	IMkit/build/outputs/aar/IMkit-release.aar

---
+++ app/src/main/java/com/example/mengmeng/activity/LoginInfo.java
@@ -0,0 +1,16 @@
+package com.example.mengmeng.activity;
+
+/**
+ * Created by admin on 2016/11/8.
+ */
+public class LoginInfo {
+
+    public static int userId=-1;
+    public static String userPhoto="";
+    public static String address="";
+    public static boolean sex=false;
+    public static String name="";
+    public static String underWrite="";
+    public static String token="";
+    public static String userPsd="";
+}
