b1c80fa8af4667325444d4342e1c8e7943c575af
coderjacky <ccy2007@yeah.net>
2016-11-14 17:56:25 +0800

    Merge branch 'master' of https://github.com/luxiansenv5/MengMeng
    Conflicts:
    	IMkit/build/intermediates/bundles/debug/classes.jar
    	IMkit/build/intermediates/bundles/debug/libs/Rong_IMKit_v2_7_2.jar
    	IMkit/build/intermediates/res/resources-debug-androidTest.ap_
    	IMkit/build/outputs/aar/IMkit-debug.aar
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/mengmeng/utils/HttpUtils.java

--- app/src/main/java/com/example/mengmeng/activity/DynamicMainActivity.java
+++ app/src/main/java/com/example/mengmeng/activity/DynamicMainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
--- app/src/main/java/com/example/mengmeng/activity/ReleaseActivity.java
+++ app/src/main/java/com/example/mengmeng/activity/ReleaseActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
